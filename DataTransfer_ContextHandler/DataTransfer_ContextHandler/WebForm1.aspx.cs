c96ef564a9e7038458adea9d4ef8cb9479d0700d
pradeep <pradeep.budhathoki1234@gmail.com>
2017-10-10 12:08:14 +0545

    another commit

---
+++ DataTransfer_ContextHandler/DataTransfer_ContextHandler/WebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DataTransfer_ContextHandler
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnWebform2_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("WebForm2.aspx");
+
+        }
+    }
+}
---
+++ DataTransfer_ContextHandler/DataTransfer_ContextHandler/WebForm2.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DataTransfer_ContextHandler
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           Page lastPage= (Page)Context.Handler;
+            lblName.Text=((TextBox)lastPage.FindControl("txtName")).Text;
+            lblEmail.Text = ((TextBox)lastPage.FindControl("txtEmail")).Text;
+        }
+    }
+}
